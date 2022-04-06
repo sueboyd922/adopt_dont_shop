1d087009588875c7836a4811bd54ea10de12b49b
Susan Boyd <sueboyd922@gmail.com>
2022-04-05 23:46:55 -0400

    Add tests in pet application model spec for all_approved and all_inspected class methods

--- spec/models/pet_applications_spec.rb
+++ spec/models/pet_applications_spec.rb
@@ -17,4 +17,50 @@ RSpec.describe PetApplication do
 
     expect(pet_app.status).to eq("Pending")
   end
+
+  describe 'class methods' do
+    before (:each) do
+      @application_1 = Application.create!(
+        name: 'Anita Barker',
+        street_address: '2468 Park Blvd.',
+        city: 'Denver',
+        state: 'CO',
+        zipcode: '80202',
+        description: 'none',
+        status: 'Pending'
+        )
+      @application_2 = Application.create!(
+        name: 'Bob Barker',
+        street_address: '1357 20th st.',
+        city: 'Golden',
+        state: 'CO',
+        zipcode: '80209',
+        description: 'na',
+        status: 'pending'
+        )
+      @shelter_1 = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      @pet_1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+      @pet_2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+    end
+
+    it 'tells you if all of the pet applications it has are approved' do
+      PetApplication.create!(pet: @pet_1, application: @application_1, status: "Approved")
+      PetApplication.create!(pet: @pet_2, application: @application_1, status: "Approved")
+      PetApplication.create!(pet: @pet_1, application: @application_2, status: "Rejected")
+      PetApplication.create!(pet: @pet_2, application: @application_2)
+
+      expect(@application_1.pet_applications.all_approved?).to be true
+      expect(@application_2.pet_applications.all_approved?).to be false
+    end
+
+    it "tells you if the all pet applications on an application have been inspected" do
+      PetApplication.create!(pet: @pet_1, application: @application_1, status: "Approved")
+      PetApplication.create!(pet: @pet_2, application: @application_1)
+      PetApplication.create!(pet: @pet_1, application: @application_2, status: "Rejected")
+      PetApplication.create!(pet: @pet_2, application: @application_2, status: "Approved")
+
+      expect(@application_1.pet_applications.all_inspected?).to be false
+      expect(@application_2.pet_applications.all_inspected?).to be true
+    end
+  end
 end
