09001c0ad5c4dcd50ccc3004f442b17952d65399
Alicia Watt <alicia.watt.aw@gmail.com>
2022-04-06 10:39:13 -0600

    fix: name is link sad path

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -1,21 +1,21 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'the admin shelters index' do
+RSpec.describe "the admin shelters index" do
   before(:each) do
-    @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    @shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
-    @shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
+    @shelter_1 = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
+    @shelter_2 = Shelter.create(name: "RGV animal shelter", city: "Harlingen, TX", foster_program: false, rank: 5)
+    @shelter_3 = Shelter.create(name: "Fancy pets of Colorado", city: "Denver, CO", foster_program: true, rank: 10)
 
-    @pet_1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
-    @pet_2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+    @pet_1 = @shelter_1.pets.create(name: "Mr. Pirate", breed: "tuxedo shorthair", age: 5, adoptable: true)
+    @pet_2 = @shelter_1.pets.create(name: "Clawdia", breed: "shorthair", age: 3, adoptable: true)
 
     @pet_3 = @shelter_2.pets.create(name: "Sherman", breed: "maine coon", age: 7, adoptable: true)
     @pet_4 = @shelter_2.pets.create(name: "Lucy", breed: "beagle", age: 2, adoptable: true)
 
-    @pet_5 = @shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+    @pet_5 = @shelter_3.pets.create(name: "Lucille Bald", breed: "sphynx", age: 8, adoptable: true)
 
-    @application_1 = Application.create!(name: "Anita Barker", street_address: '2468 Park Blvd.', city: "Denver", state: "CO", zipcode: "80202", status: "Pending")
-    @application_2 = Application.create!(name: "Frodo Baggins", street_address: '1 Shire Ave', city: "Denver", state: "CO", zipcode: "80202", status: "Rejected")
+    @application_1 = Application.create!(name: "Anita Barker", street_address: "2468 Park Blvd.", city: "Denver", state: "CO", zipcode: "80202", status: "Pending")
+    @application_2 = Application.create!(name: "Frodo Baggins", street_address: "1 Shire Ave", city: "Denver", state: "CO", zipcode: "80202", status: "Rejected")
 
     @pet_app_1 = PetApplication.create(pet: @pet_1, application: @application_1)
     @pet_app_2 = PetApplication.create(pet: @pet_2, application: @application_1)
@@ -24,16 +24,16 @@ RSpec.describe 'the admin shelters index' do
     @pet_app_5 = PetApplication.create(pet: @pet_4, application: @application_2)
   end
 
-  describe 'on index page' do
-    it 'lists all the shelters in reverse alphabetical order' do
-      visit '/admin/shelters'
+  describe "on index page" do
+    it "lists all the shelters in reverse alphabetical order" do
+      visit "/admin/shelters"
 
       expect(@shelter_2.name).to appear_before(@shelter_3.name)
       expect(@shelter_3.name).to appear_before(@shelter_1.name)
     end
   end
 
-  it 'shows the shelters with pending applications' do
+  it "shows the shelters with pending applications" do
     visit "/admin/shelters"
 
     expect(page).to have_content("Shelters with Pending Applications")
@@ -44,8 +44,8 @@ RSpec.describe 'the admin shelters index' do
     end
   end
 
-  it 'shows the shelters with pending applications in alphabetical order' do
-    @application_3 = Application.create!(name: "Ron Weasley", street_address: 'The Burrow', city: "London", state: "CO", zipcode: "80202", status: "Pending")
+  it "shows the shelters with pending applications in alphabetical order" do
+    @application_3 = Application.create!(name: "Ron Weasley", street_address: "The Burrow", city: "London", state: "CO", zipcode: "80202", status: "Pending")
     PetApplication.create!(pet: @pet_5, application: @application_3)
 
     visit "/admin/shelters"
@@ -56,14 +56,14 @@ RSpec.describe 'the admin shelters index' do
     end
   end
 
-  it 'the name is a link to their show page' do
+  it "the name is a link to their show page" do
     Shelter.all.each do |shelter|
       visit "/admin/shelters"
-      click_on("#{shelter.name}")
+      click_on(shelter.name.to_s)
       expect(current_path).to eq("/admin/shelters/#{shelter.id}")
       expect(page).to have_content(shelter.name)
       # expect(page).to have_content(shelter.city)
-      expect(page).not_to have_content(@pet_1.name)
+      expect(page).not_to have_content(@pet_5.name)
     end
   end
 end
